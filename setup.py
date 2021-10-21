4e943623a4062ee435ed3311de3560c31d122f3d
John Tamanas <8213449+jtamanas@users.noreply.github.com>
2021-10-21 09:58:31 -0700

    fixed description

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from os import path
 from setuptools import find_packages, setup
 
-exec(open("nflows/version.py").read())
-
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name="fnf4hep",
-    version=__version__,
+    version='0.0.0a',
     description="Fair Normalizing Flows for HEP",
-    long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/jtamanas/FNF4HEP",
     license="MIT",
