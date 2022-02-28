62e649ced2f82b1776c925cf621f7d603b8a203c
Nolan Smyth <nolanwsmyth@gmail.com>
2022-02-28 10:30:07 -0800

    label classifier working

--- models/classifier.py
+++ models/classifier.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
+from tqdm.auto import trange
 
 
 class BinaryClassifier(nn.Module):
-    def __init__(self, data_dim, hidden_dim=32, n_layers=2, activation='GELU'):
+    def __init__(self, data_dim, hidden_dim=32, n_layers=2, activation='ReLU'):
         super(BinaryClassifier, self).__init__()
         self.data_dim = data_dim
         self.hidden_dim = hidden_dim
@@ -17,11 +18,9 @@ class BinaryClassifier(nn.Module):
             layers.append(getattr(nn, activation)())
             
         layers.append(nn.Linear(hidden_dim, 1))
-        # layers.append(nn.Sigmoid())
         
         self.model = nn.Sequential(*layers)
         self.criterion = nn.BCEWithLogitsLoss()
-        # self.criterion = nn.BCELoss()
         
     def forward(self, x):
         return self.model(x).squeeze()
@@ -30,7 +29,28 @@ class BinaryClassifier(nn.Module):
         y = self.forward(x).squeeze()
         return self.criterion(y, labels)
 
-    def predict(self, x):
+    def predict(self, x): #Don't think I need predict
         y = self.forward(x).squeeze()
         return torch.round(y)
+
+    def fit(self, data_loader_train, n_steps = int(1e4), lr=1e-4, weight_decay=1e-4):
+
+        optimizer = torch.optim.AdamW(self.parameters(), lr=lr, weight_decay=weight_decay)  
+
+        self.train()
+        classifier_loss = []
+        print("Training Binary Label Classifier...")
+        for step in trange(n_steps):
+            data, labels = next(iter(data_loader_train)) #TODO add context to data_loader 
+            
+            optimizer.zero_grad()
+            label_loss = self.loss(data, labels).mean()
+            classifier_loss.append(label_loss.item())
+            label_loss.backward()
+            optimizer.step()
+
+            #TODO: add validation set
+        self.eval();
     
+    def accuracy(self, data_test, labels_test):
+        return ((self.forward(data_test) > 0.0) == labels_test).float().mean().item()
--- models/my_fair.py
+++ models/my_fair.py
@@ -2,81 +2,226 @@ import torch
 import torch.nn as nn
 from .flow import Flow
 from .classifier import BinaryClassifier
+from sklearn.metrics import accuracy_score
 
 
 class BinaryFair(nn.Module):
-    def __init__(self, 
-                 data_dim, 
-                 context_dim=1, 
-                 flow_hidden_dim = 32,
-                 flow_n_layers = 1,
-                 flow_transform_type = 'MaskedAffineAutoregressiveTransform', 
-                 classifier_hidden_dim = 32,
-                 classifier_n_layers = 2,
-                 classifier_activation = 'GELU',
-                 gamma=0.5,
-                ):
-        super(BinaryFair, self).__init__()
-        
+    def __init__(
+        self,
+        data_dim,
+        context_dim=None,
+        flow_hidden_dim=32,
+        flow_n_layers=1,
+        flow_transform_type="MaskedAffineAutoregressiveTransform",
+        classifier_hidden_dim=32,
+        classifier_n_layers=2,
+        classifier_activation="ReLU",
+        num_bins=0,
+        tails=None,
+        tail_bound=1.0,
+        gamma=0.5,
+    ):
+        super().__init__()
+
         self.data_dim = data_dim
         self.context_dim = context_dim
-        self.embedding_dim = data_dim  
-        
-        self.flow_hidden_dim = flow_hidden_dim 
+        self.embedding_dim = data_dim
+
+        self.flow_hidden_dim = flow_hidden_dim
         self.flow_n_layers = flow_n_layers
         self.flow_transform_type = flow_transform_type
-        
+
         self.classifier_hidden_dim = classifier_hidden_dim
         self.classifier_n_layers = classifier_n_layers
         self.classifier_activation = classifier_activation
-        
+
+        self.num_bins = num_bins
+        self.tails = tails
+        self.tail_bound = tail_bound
+
         self.gamma = gamma
-        
-        self.flow = Flow(data_dim=self.data_dim, 
-                         context_dim=self.context_dim, 
-                         hidden_dim=self.flow_hidden_dim, 
-                         n_layers=self.flow_n_layers, 
-                         transform_type=self.flow_transform_type)
-        
-        self.classifier = BinaryClassifier(self.embedding_dim)
-        
-    def forward(self, data, context, return_embedding=False):
-        embedding, log_probs = self.flow._transform(data, context=context)
-        label_pred = self.classifier(embedding)        
-        if return_embedding:
-            return label_pred, embedding
+
+        self.flow0 = Flow(
+            data_dim=self.data_dim,
+            context_dim=self.context_dim,
+            hidden_dim=self.flow_hidden_dim,
+            n_layers=self.flow_n_layers,
+            transform_type=self.flow_transform_type,
+            num_bins=self.num_bins,
+            tails=self.tails,
+            tail_bound=self.tail_bound,
+        )
+
+        self.flow1 = Flow(
+            data_dim=self.data_dim,
+            context_dim=self.context_dim,
+            hidden_dim=self.flow_hidden_dim,
+            n_layers=self.flow_n_layers,
+            transform_type=self.flow_transform_type,
+            num_bins=self.num_bins,
+            tails=self.tails,
+            tail_bound=self.tail_bound,
+        )
+
+        self.classifier = BinaryClassifier(self.embedding_dim) #Fair Classifier
+
+    def forward(self, data, context):
+        embedding0, embedding1 = self._embed(data_0=data, data_1=data)
+
+        embeddings = embedding0 * (context == 0) + embedding1 * (context == 1)
+        label_pred = self.classifier(embeddings).sigmoid()
+
         return label_pred
-    
-    def sample(self, n_samples_per_context, context=None):
-        return self.flow.sample(num_samples=n_samples_per_context, context=context)
-        
-    def _KL_loss(self, data, context, n_samples_per_context=128):        
-        flipped_context = (~context.bool()).float()
-        
-        _, correct_log_probs = self.flow._transform(data, context)
-        wrong_log_probs = self.flow.log_prob(data, flipped_context)
-        
-#         This is the way outlined in the paper, but I didn't get satisfactory results.
-        KL = correct_log_probs - wrong_log_probs
-        
-#         Instead, I'll make both conditional distributions map on the unit normal
-#         KL = zeros_log_probs - self.flow._distribution.log_prob(zeros_samples)
-#         KL += ones_log_probs - self.flow._distribution.log_prob(ones_samples)
-    
-        return KL.mean()
-    
-    def _classifier_loss(self, data, context, labels):
-        label_preds = self.forward(data, context)
+
+    def classifier_accuracy(self, embd, labels):
+        label_pred = self.classifier(embd).sigmoid()
+        bin_pred = (label_pred > 0.5).float()
+        return accuracy_score(bin_pred, labels)
+
+    def sample(self, context=None):
+        samples_0 = self.flow0.sample(num_samples=context.shape[0])
+        samples_1 = self.flow1.sample(num_samples=context.shape[0])
+
+        if context is not None:
+            context = context.bool()
+            samples = samples_0 * (~context) + samples_1 * context
+        else:
+            samples = samples_0 + samples_1
+        return samples
+
+    def _embed(self, data_0=None, data_1=None, context_0=None, context_1=None):
+        embedding0 = None
+        embedding1 = None
+
+        if data_0 is not None:
+            embedding0, logabsdet0 = self.flow0._transform(data_0, context=None)
+
+        if data_1 is not None:
+            embedding1, logabsdet1 = self.flow1._transform(data_1, context=None)
+
+        return embedding0, embedding1
+
+    def _fair_embed(self, x0=None, x1=None, context_0=None, context_1=None):
+
+        (
+            z0,
+            _,
+        ) = self.flow0._transform(x0, None)
+        (
+            z1,
+            _,
+        ) = self.flow1._transform(x1, None)
+
+        f0invz1, _ = self.flow0._transform.inverse(z1, None)
+        f1invz0, _ = self.flow1._transform.inverse(z0, None)
+
+        return f0invz1, f1invz0
+
+    def optimal_adversary(
+        self,
+        data_0,
+        data_1,
+        context_0=None,
+        context_1=None,
+        probability_flow=None,
+    ):
+        z0, _, _ = self.flow0._fair_forward(data_0, None)
+        z1, _, _ = self.flow1._fair_forward(data_1, None)
+
+        logP_Z0_z0, logP_Z1_z0 = self._log_prob(
+            z0, context_0, context_1, probability_flow
+        )
+        logP_Z0_z1, logP_Z1_z1 = self._log_prob(
+            z1, context_0, context_1, probability_flow
+        )
+
+        mu_star_0 = logP_Z1_z0 >= logP_Z0_z0
+        mu_star_1 = logP_Z1_z1 >= logP_Z0_z1
+
+        mu_star_0_avg = (mu_star_0 * 1.0).mean()
+        mu_star_1_avg = (mu_star_1 * 1.0).mean()
+
+        stat_dist = mu_star_0_avg - mu_star_1_avg
+
+        return stat_dist, mu_star_0, mu_star_1
+
+    def _log_prob(self, z, context_0=None, context_1=None, probability_flow=None):
+
+        f0invz, logdetf0invz = self.flow0._transform.inverse(z, None)
+        f1invz, logdetf1invz = self.flow1._transform.inverse(z, None)
+
+        log_P_0_z = probability_flow.log_prob(f0invz, context_0)
+        log_P_1_z = probability_flow.log_prob(f1invz, context_1)
+
+        logP_Z0_z = log_P_0_z + logdetf0invz
+        logP_Z1_z = log_P_1_z + logdetf1invz
+
+        return logP_Z0_z, logP_Z1_z
+
+    def _KL_loss(
+        self, data_0, data_1, context_0=None, context_1=None, probability_flow=None
+    ):
+        """
+        data_0: [batch_size, data_dim]
+            Data with label 0
+        data_1: [batch_size, data_dim]
+            Data with label 1
+
+        Note in the paper, they have equal numbers of each class and then take
+        the mean after adding. Here we'll take the mean first and then add scalars
+        """
+        z0, _, _ = self.flow0._fair_forward(data_0, None)
+        z1, _, _ = self.flow1._fair_forward(data_1, None)
+
+        logP_Z0_z0, logP_Z1_z0 = self._log_prob(
+            z0, context_0, context_1, probability_flow
+        )
+        logP_Z0_z1, logP_Z1_z1 = self._log_prob(
+            z1, context_0, context_1, probability_flow
+        )
+
+        L_0 = logP_Z0_z0 - logP_Z1_z0
+        L_1 = logP_Z1_z1 - logP_Z0_z1
+
+        return (L_0 + L_1).mean()
+
+    def _classifier_loss(self, embd_0, embd_1, labels_0, labels_1):
+        """
+        Not yet sure how to handle the context here.
+        """
+        labels = torch.cat([labels_0, labels_1])
+
+        embds = torch.cat([embd_0, embd_1])
+        label_preds = self.classifier.forward(embds)
+
         return self.classifier.criterion(label_preds, labels)
-    
-    def loss(self, data, context, labels, return_all_losses=False):
-        batch_size = data.shape[0]
-        
-        L_KL = self._KL_loss(data, context)
-        L_clf = self._classifier_loss(data, context, labels)
-        L_total = self.gamma*L_KL + (1. - self.gamma)*L_clf
-        
+
+    def loss(
+        self,
+        data_0,
+        data_1,
+        labels_0,
+        labels_1,
+        context_0=None,
+        context_1=None,
+        return_all_losses=False,
+        probability_flow=None,
+    ):
+        if self.gamma == 0:
+            embedding_0, embedding_1 = self._embed(data_0, data_1, context_0, context_1)
+            return (
+                torch.tensor(0),
+                self._classifier_loss(embedding_0, embedding_1, labels_0, labels_1),
+                self._classifier_loss(embedding_0, embedding_1, labels_0, labels_1),
+            )
+
+        L_KL = self._KL_loss(data_0, data_1, context_0, context_1, probability_flow)
+
+        embedding_0, embedding_1 = self._embed(data_0, data_1, context_0, context_1)
+
+        L_clf = self._classifier_loss(embedding_0, embedding_1, labels_0, labels_1)
+        L_total = self.gamma * L_KL + (1.0 - self.gamma) * L_clf
+
         if return_all_losses:
             return L_KL, L_clf, L_total
         return L_total
-    
